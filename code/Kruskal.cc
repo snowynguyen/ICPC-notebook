157d39ce2d12739a9c21b8d5c2ebef0409b809fd
bao-tg <tgb0015@gmail.com>
2025-10-04 11:54:08 +0700

    Update notebook with //Official mark in each official file

--- code/Kruskal.cc
+++ code/Kruskal.cc
@@ -1,89 +1,53 @@
-/*
-Uses Kruskal's Algorithm to calculate the weight of the minimum spanning
-forest (union of minimum spanning trees of each connected component) of
-a possibly disjoint graph, given in the form of a matrix of edge weights
-(-1 if no edge exists). Returns the weight of the minimum spanning
-forest (also calculates the actual edges - stored in T). Note: uses a
-disjoint-set data structure with amortized (effectively) constant time per
-union/find. Runs in O(E*log(E)) time.
-*/
+// Official version
+vector<int> parent, rank;
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <queue>
-
-using namespace std;
-
-typedef int T;
-
-struct edge
-{
-  int u, v;
-  T d;
-};
-
-struct edgeCmp
-{
-  int operator()(const edge& a, const edge& b) { return a.d > b.d; }
-};
-
-int find(vector <int>& C, int x) { return (C[x] == x) ? x : C[x] = find(C, C[x]); }
-
-T Kruskal(vector <vector <T> >& w)
-{
-  int n = w.size();
-  T weight = 0;
-  
-  vector <int> C(n), R(n);
-  for(int i=0; i<n; i++) { C[i] = i; R[i] = 0; }
-  
-  vector <edge> T;
-  priority_queue <edge, vector <edge>, edgeCmp> E;
-  
-  for(int i=0; i<n; i++)
-    for(int j=i+1; j<n; j++)
-      if(w[i][j] >= 0)
-      {
-        edge e;
-        e.u = i; e.v = j; e.d = w[i][j];
-        E.push(e);
-      }
-      
-  while(T.size() < n-1 && !E.empty())
-  {
-    edge cur = E.top(); E.pop();
-    
-    int uc = find(C, cur.u), vc = find(C, cur.v);
-    if(uc != vc)
-    {
-      T.push_back(cur); weight += cur.d;
-      
-      if(R[uc] > R[vc]) C[vc] = uc;
-      else if(R[vc] > R[uc]) C[uc] = vc;
-      else { C[vc] = uc; R[uc]++; }
-    }
-  }
-  
-  return weight;
+void make_set(int v) {
+    parent[v] = v;
+    rank[v] = 0;
 }
 
-int main()
-{
-  int wa[6][6] = {
-    { 0, -1, 2, -1, 7, -1 },
-    { -1, 0, -1, 2, -1, -1 },
-    { 2, -1, 0, -1, 8, 6 },
-    { -1, 2, -1, 0, -1, -1 },
-    { 7, -1, 8, -1, 0, 4 },
-    { -1, -1, 6, -1, 4, 0 } };
-    
-  vector <vector <int> > w(6, vector <int>(6));
-  
-  for(int i=0; i<6; i++)
-    for(int j=0; j<6; j++)
-      w[i][j] = wa[i][j];
-    
-  cout << Kruskal(w) << endl;
-  cin >> wa[0][0];
+int find_set(int v) {
+    if (v == parent[v])
+        return v;
+    return parent[v] = find_set(parent[v]);
+}
+
+void union_sets(int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (rank[a] < rank[b])
+            swap(a, b);
+        parent[b] = a;
+        if (rank[a] == rank[b])
+            rank[a]++;
+    }
+}
+
+struct Edge {
+    int u, v, weight;
+    bool operator<(Edge const& other) {
+        return weight < other.weight;
+    }
+};
+
+int n;
+vector<Edge> edges;
+
+int cost = 0;
+vector<Edge> result;
+parent.resize(n);
+rank.resize(n);
+for (int i = 0; i < n; i++)
+    make_set(i);
+
+sort(edges.begin(), edges.end());
+
+for (Edge e : edges) {
+    if (find_set(e.u) != find_set(e.v)) {
+        cost += e.weight;
+        result.push_back(e);
+        union_sets(e.u, e.v);
+        // if we want, we can add a trace here!!!
+    }
 }
